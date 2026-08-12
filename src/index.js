d254e7afb11c899bcbe46b6c2f9dc32d40440c79
I7xiiq <am9248968@gmail.com>
2026-08-12 18:51:51 +0300

    First commit: Node.js and Docker setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+// init app
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.get('/' , (req, res) => res.send('<h1> ed11 aييييhmeggggd</h1>'));
+
+app.listen(PORT, () => console.log(`app is up and running on port: ${PORT}`));
