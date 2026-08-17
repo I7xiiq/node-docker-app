d4cd8721770e5583dcceb537e5e4391fc85ba50a
I7xiiq <am9248968@gmail.com>
2026-08-17 06:41:22 +0300

    add postgres and stop mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const redis = require('redis');
+const { Client } = require('pg');
 
 // init app
 const PORT = process.env.PORT || 4000;
@@ -15,13 +16,43 @@ redisclient.on('connect', (err) => console.log('connected to redis' , err));
 redisclient.connect();
 
 // connect db
-const DB_USER = 'root';
-const DB_PASSWORD = 'example'
-const DB_PORT = 27017;
-const DB_HOST = 'mongo'; // أو أي اسم الخدمة كما هو في docker-compose.yml 
 
-const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-mongoose.connect(URI).then(() => console.log('connect to db...')).catch((err) => console.log('failed to connect to db: ', err));
+
+
+
+
+
+
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 5432;
+const DB_HOST = 'postgres';
+
+const URI = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+const client = new Client({
+  connectionString: URI,
+});
+
+client
+  .connect()
+  .then(() => console.log('connect to PG db...'))
+  .catch((err) => console.log('failed to connect PG to db: ', err));
+
+
+
+
+
+
+
+
+// const DB_USER = 'root';
+// const DB_PASSWORD = 'example'
+// const DB_PORT = 27017;
+// const DB_HOST = 'mongo'; // أو أي اسم الخدمة كما هو في docker-compose.yml 
+
+// const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+// mongoose.connect(URI).then(() => console.log('connect to db...')).catch((err) => console.log('failed to connect to db: ', err));
 
 app.get('/' , (req, res) => {
 redisclient.set('product' , 'product...');
